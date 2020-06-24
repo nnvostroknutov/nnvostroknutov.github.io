e7a2e327eeccc89d539bf3b025bef4a1bd7fdbd8
Nikita Vostroknutov <nnvostroknutov@yandex.ru>
2020-06-24 17:14:39 +0500

    Updates

---
+++ precache-manifest.b22087e8da4644f4dcf5f76559477dc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97522126c38b98a5d11ee2d8b8081f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3389a4ed76f1959668c",
+    "url": "/static/css/2.97d3883c.chunk.css"
+  },
+  {
+    "revision": "fc7d039818cd90465bcb",
+    "url": "/static/css/main.86cf5a5a.chunk.css"
+  },
+  {
+    "revision": "c3389a4ed76f1959668c",
+    "url": "/static/js/2.3c2ed5c9.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.3c2ed5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7d039818cd90465bcb",
+    "url": "/static/js/main.0d71380b.chunk.js"
+  },
+  {
+    "revision": "450acbb3e6981bf11f44",
+    "url": "/static/js/runtime-main.20b126ba.js"
+  },
+  {
+    "revision": "4e5bcb3bfa906050a23575ccb6bab500",
+    "url": "/static/media/graph.4e5bcb3b.PNG"
+  }
+]);
